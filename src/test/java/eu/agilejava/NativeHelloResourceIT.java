02c0269b8c7793ee848c687c20b9012ce248b906
Ivar Grimstad <ivar.grimstad@cybercom.com>
2019-04-09 11:05:40 +0200

    Super simple example created

---
+++ src/test/java/eu/agilejava/NativeHelloResourceIT.java
@@ -0,0 +1,9 @@
+package eu.agilejava;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeHelloResourceIT extends HelloResourceTest {
+
+    // Execute the same tests but in native mode.
+}
